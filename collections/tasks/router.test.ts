5362ac6e2ee1bda1d8ff7702ba0d7fa9d770806c
Jason Godesky <jefgodesky@gmail.com>
2025-01-05 16:54:32 -0500

    GET /v1/tasks/:taskId

--- collections/tasks/router.test.ts
+++ collections/tasks/router.test.ts
@@ -2,6 +2,7 @@ import { describe, afterEach, afterAll, it } from '@std/testing/bdd'
 import { expect } from '@std/expect'
 import supertest from 'supertest'
 import DB from '../../DB.ts'
+import setupTask from '../../utils/testing/setup-task.ts'
 import setupUser from '../../utils/testing/setup-user.ts'
 import getSupertestRoot from '../../utils/testing/get-supertest-root.ts'
 
@@ -61,6 +62,81 @@ describe('/tasks', () => {
         expect(res.body.data.type).toBe('tasks')
         expect(res.body.data.id).toBeDefined()
         expect(res.body.data.attributes.name).toBe(body.data.attributes.name)
+      })
+    })
+  })
+
+  describe('Resource [/tasks/:taskId]', () => {
+    describe('GET', () => {
+      it('returns 401 if user is not authenticated', async () => {
+        const { task } = await setupTask()
+        const res = await supertest(getSupertestRoot())
+          .get(`/tasks/${task.id}`)
+          .set({ 'Content-Type': 'application/vnd.api+json' })
+
+        expect(res.status).toBe(401)
+      })
+
+      it('returns 403 if you try to get someone else\'s task', async () => {
+        const { task } = await setupTask()
+        const { jwt } = await setupUser()
+        const res = await supertest(getSupertestRoot())
+          .get(`/tasks/${task.id}`)
+          .set({
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/vnd.api+json'
+          })
+
+        expect(res.status).toBe(403)
+      })
+
+      it('returns 404 if the task cannot be found', async () => {
+        const { jwt } = await setupUser()
+        const res = await supertest(getSupertestRoot())
+          .get(`/tasks/${crypto.randomUUID()}`)
+          .set({
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/vnd.api+json'
+          })
+
+        expect(res.status).toBe(404)
+      })
+
+      it('returns the task', async () => {
+        const { task, jwt } = await setupTask()
+        const res = await supertest(getSupertestRoot())
+          .get(`/tasks/${task.id}`)
+          .set({
+            Authorization: `Bearer ${jwt}`,
+            'Content-Type': 'application/vnd.api+json'
+          })
+
+        expect(res.status).toBe(200)
+        expect(res.body.data.type).toBe('tasks')
+        expect(res.body.data.id).toBe(task.id)
+        expect(res.body.data.attributes.name).toBe(task.name)
+      })
+
+      it('supports sparse fieldsets', async () => {
+        const { task, jwt } = await setupTask()
+        const fieldsets = [
+          ['name', task.name, undefined],
+          ['notes', undefined, task.notes],
+          ['name,notes', task.name, task.notes]
+        ]
+
+        for (const [q, name, notes] of fieldsets) {
+          const url = `/tasks/${task.id}?fields[tasks]=${q}`
+          const res = await supertest(getSupertestRoot())
+            .get(url)
+            .set({
+              Authorization: `Bearer ${jwt}`,
+              'Content-Type': 'application/vnd.api+json'
+            })
+
+          expect(res.body.data.attributes.name).toBe(name)
+          expect(res.body.data.attributes.notes).toBe(notes)
+        }
       })
     })
   })
--- collections/tasks/router.ts
+++ collections/tasks/router.ts
@@ -2,9 +2,11 @@ import { Router } from '@oak/oak'
 import TaskController from './controller.ts'
 import getPrefix from '../../utils/get-prefix.ts'
 import loadClient from '../../middlewares/load/client.ts'
+import loadResource from '../../middlewares/load/resource.ts'
 import requireClient from '../../middlewares/require/client.ts'
 import requireTaskCreationBody from '../../middlewares/require/body/task-creation.ts'
 import requirePermissions from '../../middlewares/require/permissions.ts'
+import requireTask from '../../middlewares/require/resources/task.ts'
 
 const router = new Router({
   prefix: getPrefix('tasks')
@@ -17,6 +19,16 @@ router.post('/',
   requirePermissions('task:create'),
   async ctx => {
     await TaskController.create(ctx)
+  })
+
+router.get('/:taskId',
+  loadClient,
+  requireClient,
+  loadResource,
+  requireTask,
+  requirePermissions('task:read'),
+  ctx => {
+    TaskController.get(ctx)
   })
 
 export default router
